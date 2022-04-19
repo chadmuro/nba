93a159ee5999bbb8428cafb80c9ac205a451b9f0
chadmuro <chadmuro@gmail.com>
2022-04-19 22:46:12 +0900

    add header and container for layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
 </script>
 
-<slot />
+<Header />
+<main class="max-w-7xl mx-auto p-4">
+	<slot />
+</main>
---
+++ src/routes/auth.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<form class="form-control w-full max-w-xs">
+	<label class="label" for="email">
+		<span class="label-text">Email</span>
+	</label>
+	<input
+		id="email"
+		type="email"
+		placeholder="Email address"
+		class="input input-primary input-bordered w-full max-w-xs"
+	/>
+</form>
