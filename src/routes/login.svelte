7f236e0233ad3f8ccba0647132ad90fed4275360
chadmuro <chadmuro@gmail.com>
2022-04-24 18:52:52 +0900

    fix ui for login and signup pages

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -23,29 +23,31 @@
 	};
 </script>
 
-<form class="form-control w-full max-w-xs" on:submit|preventDefault={handleLogin}>
-	<h1>Welcome to NBA Pick'em</h1>
-	<p class="description">Login with your email below</p>
-	<label class="label" for="email">
-		<span class="label-text">Email</span>
-	</label>
-	<input
-		bind:value={email}
-		id="email"
-		type="email"
-		placeholder="Email address"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<label class="label" for="password">
-		<span class="label-text">Password</span>
-	</label>
-	<input
-		bind:value={password}
-		id="password"
-		type="password"
-		placeholder="Password"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<button class={`btn btn-primary my-4 ${loading && 'loading'}`} type="submit">Submit</button>
-	<p>Don't have an account? <a href="/signup" class="link link-primary">Sign up</a></p>
-</form>
+<div class="mx-auto flex justify-center max-w-md border border-gray-500 p-8 rounded">
+	<form class="w-full form-control" on:submit|preventDefault={handleLogin}>
+		<h1 class="text-2xl text-center">Welcome to NBA Pick'em</h1>
+		<p class="description my-4 text-center">Login with your email below</p>
+		<label class="label" for="email">
+			<span class="label-text">Email</span>
+		</label>
+		<input
+			bind:value={email}
+			id="email"
+			type="email"
+			placeholder="Email address"
+			class="input input-primary input-bordered w-full"
+		/>
+		<label class="label" for="password">
+			<span class="label-text">Password</span>
+		</label>
+		<input
+			bind:value={password}
+			id="password"
+			type="password"
+			placeholder="Password"
+			class="input input-primary input-bordered w-full"
+		/>
+		<button class={`btn btn-primary my-4 ${loading && 'loading'}`} type="submit">Submit</button>
+		<p>Don't have an account? <a href="/signup" class="link link-primary">Sign up</a></p>
+	</form>
+</div>
--- src/routes/signup.svelte
+++ src/routes/signup.svelte
@@ -1,7 +1,6 @@
 <script>
 	import supabase from '$lib/supabaseClient';
 	import { goto } from '$app/navigation';
-	import { load } from './teams/index.svelte';
 
 	let loading = false;
 	let email;
@@ -24,29 +23,31 @@
 	};
 </script>
 
-<form class="form-control w-full max-w-xs" on:submit|preventDefault={handleLogin}>
-	<h1>Welcome to NBA Pick'em</h1>
-	<p class="description">Sign up for a new account with your email below</p>
-	<label class="label" for="email">
-		<span class="label-text">Email</span>
-	</label>
-	<input
-		bind:value={email}
-		id="email"
-		type="email"
-		placeholder="Email address"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<label class="label" for="password">
-		<span class="label-text">Password</span>
-	</label>
-	<input
-		bind:value={password}
-		id="password"
-		type="password"
-		placeholder="Password"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<button class={`btn btn-primary my-4 ${loading && 'loading'}`} type="submit">Submit</button>
-	<p>Already have an account? <a href="/login" class="link link-primary">Login</a></p>
-</form>
+<div class="mx-auto flex justify-center max-w-md border border-gray-500 p-8 rounded">
+	<form class="form-control w-full" on:submit|preventDefault={handleLogin}>
+		<h1 class="text-2xl text-center">Welcome to NBA Pick'em</h1>
+		<p class="description my-4 text-center">Sign up for a new account with your email below</p>
+		<label class="label" for="email">
+			<span class="label-text">Email</span>
+		</label>
+		<input
+			bind:value={email}
+			id="email"
+			type="email"
+			placeholder="Email address"
+			class="input input-primary input-bordered w-full"
+		/>
+		<label class="label" for="password">
+			<span class="label-text">Password</span>
+		</label>
+		<input
+			bind:value={password}
+			id="password"
+			type="password"
+			placeholder="Password"
+			class="input input-primary input-bordered w-full"
+		/>
+		<button class={`btn btn-primary my-4 ${loading && 'loading'}`} type="submit">Submit</button>
+		<p>Already have an account? <a href="/login" class="link link-primary">Login</a></p>
+	</form>
+</div>
