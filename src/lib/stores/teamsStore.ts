2aa1fa8d27de9a45c68ff2957776ff59540ebbe2
chadmuro <chadmuro@gmail.com>
2022-05-03 12:16:25 +0900

    create teams store and add teams to profile page

---
+++ src/lib/stores/teamsStore.ts
@@ -0,0 +1,22 @@
+import supabase from "$lib/supabaseClient";
+import { writable } from "svelte/store";
+
+export const teams = writable([]);
+
+export async function getTeams() {
+  console.log("data fetch");
+  const { data, error } = await supabase
+    .from('teams')
+    .select(
+      `*, division (name),
+  conference (name)`
+    )
+    .order('full_name', { ascending: true });
+
+  if (error) {
+    console.error(error);
+  }
+  teams.set(data);
+}
+
+getTeams();
--- src/routes/profile.svelte
+++ src/routes/profile.svelte
@@ -1,5 +1,11 @@
 <script context="module">
 	export async function load({ session }) {
+		if (!session) {
+			return {
+				status: 302,
+				redirect: '/login'
+			};
+		}
 		let {
 			data: profile,
 			error,
@@ -22,6 +28,7 @@
 	import supabase from '$lib/supabaseClient';
 	import countries from '$lib/countries';
 	import { session } from '$app/stores';
+	import { teams } from '$lib/stores/teamsStore';
 
 	export let profile;
 
@@ -57,43 +64,48 @@
 	}
 </script>
 
-<form class="form-control w-full max-w-xs" on:submit|preventDefault={updateProfile}>
-	<label class="label" for="username">
-		<span class="label-text">Username</span>
-	</label>
-	<input
-		bind:value={username}
-		id="username"
-		type="text"
-		placeholder="Username"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<label class="label" for="country">
-		<span class="label-text">Country</span>
-	</label>
-	<select bind:value={country} class="select select-primary w-full max-w-xs" id="country">
-		<option disabled value={null} selected>Country</option>
-		{#each Object.values(countries) as country}
-			<option value={`${country.emoji} ${country.name}`}>{country.emoji} {country.name}</option>
-		{/each}
-	</select>
-	<label class="label" for="favorite_team">
-		<span class="label-text">Favorite Team</span>
-	</label>
-	<select bind:value={favorite_team} class="select select-primary w-full max-w-xs" id="country">
-		<option disabled selected>Team</option>
-		<!-- {#each Object.values(countries) as country}
-			<option value={`${country.emoji} ${country.name}`}>{country.emoji} {country.name}</option>
-		{/each} -->
-	</select>
-	<label class="label" for="image">
-		<span class="label-text">Profile</span>
-	</label>
-	<input
-		id="image"
-		type="file"
-		placeholder="Profile"
-		class="input input-primary input-bordered w-full max-w-xs"
-	/>
-	<button class={`btn btn-primary my-4 ${loading && 'loading'}`}>Submit</button>
-</form>
+<h1>My Profile</h1>
+<div class="mx-auto flex justify-center max-w-md border border-gray-500 p-8 rounded">
+	<form class="form-control w-full max-w-xs" on:submit|preventDefault={updateProfile}>
+		<label class="label" for="username">
+			<span class="label-text">Username</span>
+		</label>
+		<input
+			bind:value={username}
+			id="username"
+			type="text"
+			placeholder="Username"
+			class="input input-primary input-bordered w-full max-w-xs"
+		/>
+		<label class="label" for="country">
+			<span class="label-text">Country</span>
+		</label>
+		<select bind:value={country} class="select select-primary w-full max-w-xs" id="country">
+			<option disabled value={null} selected>Country</option>
+			{#each Object.values(countries) as country}
+				<option value={`${country.emoji} ${country.name}`}>{country.emoji} {country.name}</option>
+			{/each}
+		</select>
+		<label class="label" for="favorite_team">
+			<span class="label-text">Favorite Team</span>
+		</label>
+		<select bind:value={favorite_team} class="select select-primary w-full max-w-xs" id="country">
+			<option disabled selected>Team</option>
+			{#each $teams as team}
+				<option value={team.id}>
+					{team.full_name}
+				</option>
+			{/each}
+		</select>
+		<label class="label" for="image">
+			<span class="label-text">Profile</span>
+		</label>
+		<input
+			id="image"
+			type="file"
+			placeholder="Profile"
+			class="input input-primary input-bordered w-full max-w-xs"
+		/>
+		<button class={`btn btn-primary my-4 ${loading && 'loading'}`}>Submit</button>
+	</form>
+</div>
