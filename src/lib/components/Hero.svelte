dc660af2eb5d2bc0b0be792557dfead72185881a
chadmuro <chadmuro@gmail.com>
2022-05-29 15:04:59 +0900

    add coming soon pages

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { session } from '$app/stores';
+	console.log(session);
 </script>
 
 <div class="bg-base-200">
@@ -11,8 +12,8 @@
 					Welcome to NBA Pick'em. Choose a winner every night based on the line and climb the
 					leaderboard!
 				</p>
-				<a href={session ? '/leaderboard' : '/login'} class="btn btn-primary"
-					>{session ? 'Check out the leaderboard' : 'Get Started'}</a
+				<a href={$session ? '/leaderboard' : '/login'} class="btn btn-primary"
+					>{$session ? 'Check out the leaderboard' : 'Get Started'}</a
 				>
 			</div>
 		</div>
